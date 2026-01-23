6da99b6370a6583cbd2b8349ca2bbcc90f28eee5
amitbossff <amitkr706186@gmail.com>
2026-01-23 09:59:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBxSL_ktKCcnkEvWUjHsQojz-JOVLSTO3s",
-  authDomain: "amitbhai-a77a1.firebaseapp.com",
-  databaseURL: "https://amitbhai-a77a1-default-rtdb.firebaseio.com",
-  projectId: "amitbhai-a77a1",
-  storageBucket: "amitbhai-a77a1.appspot.com",
-  messagingSenderId: "587608990593",
-  appId: "1:587608990593:web:a5f4a8dbca029e30bcbe80"
-};
-
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-
 function searchYT(e) {
   e.preventDefault();
 
@@ -25,32 +12,13 @@ function searchYT(e) {
 
 function clearInput() {
   const input = document.getElementById("query");
-  const btn = document.getElementById("clearBtn");
-
   input.value = "";
   input.focus();
-
-  btn.classList.add("active-yellow");
-  setTimeout(() => btn.classList.remove("active-yellow"), 600);
+  toggleClear();
 }
 
-function moveUp() {
-  document.getElementById("appShell").style.transform =
-    "translateY(-110px)";
+function toggleClear() {
+  const input = document.getElementById("query");
+  const btn = document.getElementById("clearBtn");
+  btn.style.display = input.value ? "flex" : "none";
 }
-
-function moveDown() {
-  document.getElementById("appShell").style.transform =
-    "translateY(0)";
-}
-
-const userRef = db.ref("onlineUsers").push();
-userRef.set(true);
-userRef.onDisconnect().remove();
-
-db.ref("onlineUsers").on("value", snapshot => {
-  const el = document.getElementById("onlineUsers");
-  if (el) {
-    el.innerText = snapshot.numChildren();
-  }
-});
